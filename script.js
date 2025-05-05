7e8c2fe2c8ecb4cd9529d147171fccc256eee8e3
waiorecchi <tatukikitamura123456@gmail.com>
2025-05-05 13:21:47 +0900

    at ドトール

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
         changeText.style.backgroundColor = randomColor;
     });
 });
+
+function myFunction(){
+    var x =document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className ="topnav";
+    }
+}
