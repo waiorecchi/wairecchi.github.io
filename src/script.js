d2604df81c6e105b8a6e78048e804343a0499c28
waiorecchi <tatukikitamura123456@gmail.com>
2025-05-15 23:44:48 +0900

    cursorかみ！

--- src/script.js
+++ src/script.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "topnav";
   }
